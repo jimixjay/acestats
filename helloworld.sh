0d38cf3e31485fea3fe63816e5cbbdd01fd7cc6e
jimixjay <jimixjay@gmail.com>
2020-10-24 17:12:54 +0200

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "Hello, worldJAIME!"
+echo "Hello, worldJAIME! v"
 
 echo "The time is $(date)."
